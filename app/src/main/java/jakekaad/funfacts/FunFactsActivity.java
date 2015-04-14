64f5305842d8ff44c80f5c6043fdbbaeed62a6ea
Jake Kaad <kaadalac@gmail.com>
2015-04-14 16:06:45 -0700

    Add random background colors via colorWheel class

--- app/src/main/java/jakekaad/funfacts/FunFactsActivity.java
+++ app/src/main/java/jakekaad/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package jakekaad.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,6 +14,7 @@ import java.util.Random;
 public class FunFactsActivity extends ActionBarActivity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,17 @@ public class FunFactsActivity extends ActionBarActivity {
 
         // Declare our view variables and assign them the Views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = mFactBook.getFact();
                 factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
